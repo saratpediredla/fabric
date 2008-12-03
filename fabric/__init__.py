ba05ae8ed2e1432fff779644ae535acc433d8448
Christian Vest Hansen <karmazilla@gmail.com>
2008-12-03 19:16:59 +0100

    shine up public package interface

--- fabric/__init__.py
+++ fabric/__init__.py
@@ -1,5 +1,20 @@
 
+# Core interface:
 from core import main, Fabric
 
-__all__ = ['main', 'Fabric']
+# Utilities and helper interface:
+import netio
+import util
+import interop
+
+# Base functionality interface:
+import core_plugin_decs as base_decorators
+import core_plugin_ops as base_operations
+import core_plugin_cmds as base_commands
+
+__all__ = [
+    'main', 'Fabric',
+    'netio', 'util', 'interop',
+    'base_decorators', 'base_operations', 'base_commands',
+]
 
